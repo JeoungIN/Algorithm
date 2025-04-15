96a717b1dde0544adf9983a2cdb803b35c888ba0
JeoungIN <113427575+JeoungIN@users.noreply.github.com>
2025-04-15 13:57:49 +0900

    [Gold V] Title: 회의실 배정, Time: 464 ms, Memory: 43880 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N;
+    public static int[][] time;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        time = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int count = 0;
+        int end = 0;
+
+        for(int i = 0; i < N; i++) {
+            if(end <= time[i][0]) {
+                end = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
